c479eab878da9ecf7aa07f7d2844fd027f745cff
16789 <16789@LAPTOP-EDER7MN8>
2019-10-07 00:14:08 -0400

    hello world program

---
+++ helloWorld/src/MyClass.java
@@ -0,0 +1,22 @@
+
+public class MyClass {
+	int x;
+	int y;
+	MyClass(int x, int y) {
+		this.x=x;
+		this.y=y;
+		int z = x + y;
+		System.out.println(z);
+				
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  System.out.println("hello world");
+  MyClass ojb= new MyClass(5,4);
+
+  
+	}
+
+}
